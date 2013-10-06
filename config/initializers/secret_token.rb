2161436a01b7878031ada0d8ce4928802035cee6
dmytro <dmytro.solonina@gmail.com>
2013-10-07 01:54:46 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Filato::Application.config.secret_key_base = 'a5917e268f9ce9fea6c381ecd9e96db301fa2997b7ab199235cad6103a19cb95209a5701c14e7585cc57984ab6b591c2f20eb9ed84f75c12b5a2492fc663a0ee'
